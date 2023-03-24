3e7d1fa5b755e2fb9cd0e182127828355417a076
Ian Richard <ian.orieko@student.moringaschool.com>
2023-03-24 13:28:44 +0000

    Completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,15 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
-    render json: students
+    if params[:name] != nil
+      render json: Student.where("first_name = ? OR last_name = ?", params[:name].capitalize, params[:name].capitalize)
+    else
+      render json: students
+    end
+  end
+
+  def show
+    render json: Student.find(params[:id])
   end
 
 end
